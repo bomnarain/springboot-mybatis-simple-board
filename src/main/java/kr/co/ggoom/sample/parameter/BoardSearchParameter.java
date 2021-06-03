233b5b7bee1c34c18f102279275bc833a3602285
DESKTOP-KDCM8IF\HOTELGA <bomnarain@hotelga.co.kr>
2021-06-03 17:44:36 +0900

    SearchParamer 배열로 검색 기능 구현

--- src/main/java/kr/co/ggoom/sample/parameter/BoardSearchParameter.java
+++ src/main/java/kr/co/ggoom/sample/parameter/BoardSearchParameter.java
@@ -1,11 +1,15 @@
 package kr.co.ggoom.sample.parameter;
 
+import java.util.List;
+
+import kr.co.ggoom.sample.domain.BoardType;
 import lombok.Data;
 
 @Data
 public class BoardSearchParameter {
 
 	private String keyword;
+	private List<BoardType> boardTypes;
 
 	public BoardSearchParameter() {
 		
